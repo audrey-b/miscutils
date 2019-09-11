db12aa2f1708a691a565b41c02bba1385dfd72bf
audrey-b <audrey.beliveau@uwaterloo.ca>
2019-09-11 11:00:43 -0400

    output_format not an argument of render2; added preview argument

--- R/render2.R
+++ R/render2.R
@@ -1,20 +1,20 @@
 #' A function to render and preview markdown documents.
 #'
-#' This function provides a solution to the knit button in RStudio, which fails when working from a mapped disk. It has the
-#' advantage over the rmarkdown::render function to provide a preview of the rendered document.
+#' This function provides an alternative to the Knit button in RStudio, which tends to fail when working from a mapped disk. 
+#' It is based on the \code{\link[rmarkdown]{render}} function but also provides a preview of the rendered document like Knit.
 #' 
 #' @param input The path of the input file to be rendered. Generally an R Markdown document (.Rmd). Defaults to the active file in the RStudio editor.
 #' @param replace A list of two strings. The first string will be replaced by the second one in the path. This can be used to solve issues when working from a mapped disk.
-#' @param ... Arguments to pass to rmarkdown::render.
+#' @param preview Whether to preview the rendered documents.
+#' @param ... Arguments to pass to \code{\link[rmarkdown]{render}}. For example, output_format="all" will render all formats defined within the file.
 #' 
 #' @export
 #'
-#'
-#'
-#'
+
 
 render2 <- function(input = rstudioapi::getSourceEditorContext()$path,
                     replace = getOption("path.replace.list"),
+                    preview = TRUE,
                     ...){
   
   filename <- basename(input)
@@ -31,13 +31,14 @@ render2 <- function(input = rstudioapi::getSourceEditorContext()$path,
   tempDir <- tempfile()
   dir.create(tempDir)
   
-  rmarkdown::render(newinput, output_dir = tempDir, output_format = "all", ...)
+  rmarkdown::render(newinput, output_dir = tempDir, ...)
   renderedFiles <- list.files(tempDir, full.names = TRUE)
   file.copy(renderedFiles, newdir, overwrite=TRUE)
   
+  if(preview==TRUE){
   viewer <- getOption("viewer")
-  
   for(i in 1:length(renderedFiles)) viewer(renderedFiles[[i]])
+  }
   
 }
 
