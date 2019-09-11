e858d2f600d27426d75eef76d166a175559e3165
audrey-b <audrey.beliveau@uwaterloo.ca>
2019-09-11 09:57:31 -0400

    render2 did not work when other files had to be accessed. Changed render2 so that the problematic path can be changed in options(). rstudioapi is used.

--- R/render2.R
+++ R/render2.R
@@ -1,36 +1,49 @@
 #' A function to render and preview markdown documents.
 #'
-#' This function provides a solution to the knit button in RStudio, which fails when working from a mounted disk. It has the
+#' This function provides a solution to the knit button in RStudio, which fails when working from a mapped disk. It has the
 #' advantage over the rmarkdown::render function to provide a preview of the rendered document.
 #' 
-#' @param filename The name of the input file to be rendered. Generally an R Markdown document (.Rmd).
-#' @param dir The directory where the input file is located.
-#' @param output_format The R Markdown output format to convert to. Currently only "pdf_document" and "html_document" are implemented.
+#' @param input The path of the input file to be rendered. Generally an R Markdown document (.Rmd). Defaults to the active file in the RStudio editor.
+#' @param replace A list of two strings. The first string will be replaced by the second one in the path. This can be used to solve issues when working from a mapped disk.
+#' @param output_format The R Markdown output format to convert to. Currently only "pdf_document" and "html_document" are supported
 #' @param ... Arguments to pass to rmarkdown::render.
 #' 
 #' @export
 #'
+#'
+#'
+#'
 
-render2 <- function(filename, dir = getwd(), output_format = "pdf_document", ...){
+render2 <- function(input = rstudioapi::getSourceEditorContext()$path,
+                    replace = getOption("path.replace.list"),
+                    output_format = "pdf_document", 
+                    ...){
   
   if(output_format == "pdf_document") format<-"pdf"
   if(output_format == "html_document") format<-"html"
   
-  path <- file.path(dir, filename)
+  filename <- basename(input)
+  filenameraw <- sub(pattern = "(.*)\\..*$", replacement = "\\1", filename)
+  dir <- normalizePath(dirname(input), winslash = "/") 
   
-  filename <- sub(pattern = "(.*)\\..*$", replacement = "\\1", basename(filename))
+  if(!is.null(replace)){
+    newdir <- sub(replace[[1]], replace[[2]], dir, fixed=TRUE)
+    newdir <- sub("\\\\", "", newdir, fixed=TRUE)
+  }
   
-  file <- basename(path)
-  tempDir <- tempfile()
-  dir.create(tempDir)
-  newPath <- file.path(tempDir, file)
-  file.copy(path, newPath)
+  renderedPath <- file.path(newdir, paste0(filenameraw, ".", format))
+  newinput <- file.path(newdir, filename)
   
-  rmarkdown::render(newPath, output_format = output_format, ...)
-  renderedPath <- file.path(tempDir, paste0(filename, ".", format))
-  file.copy(renderedPath, file.path(dir, paste0(filename, ".", format)))
+  rmarkdown::render(newinput, output_format = output_format, ...)
+  # file <- basename(renderedPath)
+  # tempDir <- tempfile()
+  # dir.create(tempDir)
+  # renderedFile <- file.path(tempDir, file)
+  # file.copy(renderedPath, renderedFile)
+  # viewer <- getOption("viewer")
+  # viewer(renderedFile)
   
   viewer <- getOption("viewer")
   viewer(renderedPath)
-  
 }
+
